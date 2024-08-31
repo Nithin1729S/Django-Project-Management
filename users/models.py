8f709d472febd93062a952b747d7f072888b35f5
Nithin1729S <sureshnithin1729@gmail.com>
2024-09-01 03:16:26 +0530

    signals

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+
 import uuid
 # Create your models here.
 class Profile(models.Model):
@@ -19,7 +20,7 @@ class Profile(models.Model):
     id = models.UUIDField(default=uuid.uuid4,unique=True,primary_key=True,editable=False)
 
     def __str__(self):
-        return str(self.user.username)
+        return str(self.username)
 
 
 class Skill(models.Model):
