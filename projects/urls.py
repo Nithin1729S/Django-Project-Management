33beb9c7024e6541e649d640ec0cff5dfa316795
Nithin1729S <sureshnithin1729@gmail.com>
2024-09-01 02:28:40 +0530

    stuff

--- projects/urls.py
+++ projects/urls.py
@@ -5,5 +5,5 @@ urlpatterns=[
     path('project/<str:pk>/', views.project, name='project'),
     path('create-project/',views.createProject,name='create-project'),
     path('update-project/<str:pk>/',views.updateProject,name='update-project'),
-    path('eelete-project/<str:pk>/',views.deleteProject,name='delete-project')
+    path('delete-project/<str:pk>/',views.deleteProject,name='delete-project')
 ]
